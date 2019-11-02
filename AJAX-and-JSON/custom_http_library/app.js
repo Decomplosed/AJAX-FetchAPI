bba3c2e6c9fcfad583cc8e7eb5d69a1cdcb10025
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-02 01:02:10 +0100

    added POST PUT and DELETE methods to the library

--- AJAX-and-JSON/custom_http_library/app.js
+++ AJAX-and-JSON/custom_http_library/app.js
@@ -1,9 +1,38 @@
 const http = new easyHttp
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// })
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a Custom Post'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
   if(err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 })
